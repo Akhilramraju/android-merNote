c7671e0762664f34b6f0d44142947d9b16727e3b
Akhil Ram Raju Nannapuraju <akhil8099123@gmail.com>
2021-02-14 15:15:30 +1100

    Added android Test implementation using view Actions

--- app/src/test/java/com/android/mernote/DataManagerTest.java
+++ app/src/test/java/com/android/mernote/DataManagerTest.java
@@ -2,32 +2,64 @@ package com.android.mernote;
 
 import junit.framework.TestCase;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class DataManagerTest extends TestCase {
 
+    static DataManager sDataManager;
+
+     @BeforeClass
+    public static  void classSetup(){
+        sDataManager = DataManager.getInstance();
+    }
+
+    @Before
+    public void setUp(){
+        sDataManager = DataManager.getInstance();
+        sDataManager.getNotes().clear();
+        sDataManager.initializeExampleNotes();
+    }
+
     @Test
     public void testCreateNewNote() {
-        DataManager dm = DataManager.getInstance();
-        final CourseInfo course = dm.getCourse("android_async");
+
+        final CourseInfo course = sDataManager.getCourse("android_async");
         final String noteTitle = "Test note title";
         final String noteText = "This is the body of the test note";
 
-        int noteIndex = dm.createNewNote();
-        NoteInfo newNote = dm.getNotes().get(noteIndex);
+        int noteIndex = sDataManager.createNewNote();
+        NoteInfo newNote = sDataManager.getNotes().get(noteIndex );
 
         newNote.setCourse(course);
         newNote.setTitle(noteTitle);
         newNote.setText(noteText);
 
 
-        NoteInfo compareNote = dm.getNotes().get(noteIndex);
+        NoteInfo compareNote = sDataManager.getNotes().get(noteIndex);
 
-        assertEquals(compareNote.getCourse(),course);
-        assertEquals(compareNote.getTitle(),noteTitle);
-        assertEquals(compareNote.getText(),noteText);
+        assertEquals(course,compareNote.getCourse());
+        assertEquals(noteTitle,compareNote.getTitle());
+        assertEquals(noteText,compareNote.getText());
 
     }
+    @Test
+    public  void testCreateNewNoteOneSteppCreation(){
+        final  CourseInfo course = sDataManager.getCourse("android_async");
+        final String noteTitle = "Test note Title";
+        final String noteText = "This is the body of my test note";
+
+        int noteIndex = sDataManager.createNewNote(course,noteTitle,noteText);
+
+        NoteInfo compareNote = sDataManager.getNotes().get(noteIndex);
+
+        assertEquals(course,compareNote.getCourse());
+        assertEquals(noteTitle,compareNote.getTitle());
+        assertEquals(noteText,compareNote.getText());
+    }
+
+
 
 }
