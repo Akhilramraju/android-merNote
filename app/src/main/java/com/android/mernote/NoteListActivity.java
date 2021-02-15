bc7233c1014a4ad3b38702a47f332d80767add4f
Akhil Ram Raju Nannapuraju <akhil8099123@gmail.com>
2021-02-15 13:13:47 +1100

    Data binding with the recycler view from Data manager using RecyclerAdapter

--- app/src/main/java/com/android/mernote/NoteListActivity.java
+++ app/src/main/java/com/android/mernote/NoteListActivity.java
@@ -79,6 +79,10 @@ public class NoteListActivity extends AppCompatActivity {
         final LinearLayoutManager notesLayoutManager = new LinearLayoutManager(this);
         recyclerNotes.setLayoutManager(notesLayoutManager);
 
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        final NoteRecyclerAdapter noteRecyclerAdapter = new NoteRecyclerAdapter(this,notes);
+        recyclerNotes.setAdapter(noteRecyclerAdapter);
+
 
     }
 }
